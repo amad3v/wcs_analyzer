ce13b607f264b47c5045940e53249886965a4b3c
amad3v <55244128+amad3v@users.noreply.github.com>
2019-12-04 23:54:55 +0000

    Update params.py

--- mods/params.py
+++ mods/params.py
@@ -1,4 +1,5 @@
-import json
+"""Manage application settings"""
+import shelve
 from pathlib import Path
 
 
@@ -7,38 +8,53 @@ class Settings:
 
     def __init__(self):
         # os.path.expanduser('~') # = os.path.dirname(os.path.realpath(__file__))
+        # initial path set to HOME (for saving settings)
         self.__settings_path = Path.home()
         # os.path.join(self.__settings_path, 'settings.json')
-        self.__settings_file = self.__settings_path.joinpath('settings.json')
+        # settings file (full path) .../settings.db
+        self.__settings_file = self.__settings_path.joinpath('settings')
         self.__exist = False
-        self.file_exit()
-        # default path, in case settings.json doesn't exist
+        self.__file_exit()
+        # default path, in case settings.dat doesn't exist (e.g. 1st launch)
+        # joins the path in __settings_path to Desktop folder
         self.__fail_safe_path = str(self.__settings_path.joinpath('Desktop'))
-        # if os.name=='posix':
-        #     self.__fail_safe_path = os.path.expanduser("~/Desktop")
-        # if os.name=='nt':
-        #     self.__fail_safe_path = os.path.expanduser("~\\Desktop")
+        # open: last opened directory (input files / open)
+        # save: last opened directory (output file / save)
         self.__paths = {}  # {'open':'','save':''}
-        self.load_settings()
+        self.__load_settings()
 
-    def file_exit(self):
+    def __file_exit(self):
         # check if the file exists
         # self.__exist = os.path.isfile(self.__settings_file)
-        self.__exist = self.__settings_file.is_file()
+        self.__exist = self.__settings_path.joinpath('settings.dat').is_file()
 
-    def load_settings(self):
+    def __load_settings(self):
         if self.__exist:
-            with open(str(str(self.__settings_file)), 'r') as file:
-                self.__paths = json.load(file)
+            with shelve.open(str(self.__settings_file)) as db:
+                self.__paths = {'open': db['open'], 'save': db['save']}
         else:
             self.__paths = {'open': self.__fail_safe_path, 'save': self.__fail_safe_path}
 
-    def save_settings(self):
-        with open(str(self.__settings_file), 'w') as file:
-            json.dump(self.__paths, file)
+    def __save_settings(self):
+        with shelve.open(str(self.__settings_file)) as db:
+            db['open'] = self.__paths['open']
+            db['save'] = self.__paths['save']
 
-    def set_path(self, typ, path):
-        self.__paths[typ] = str(Path(path).parent)
+    def set_path(self, dialog_type, path):
+        """Save a directory path.
 
-    def get_path(self, typ):
-        return self.__paths[typ]
+        Keyword arguments:
+        dialog_type -- 'open' or 'save' (string)
+        path -- path to save (string)
+        """
+        self.__paths[dialog_type] = str(Path(path).parent)
+        self.__save_settings()
+
+    def get_path(self, dialog_type):
+        """Return a directory path.
+
+        Keyword arguments:
+        dialog_type -- 'open' or 'save' (string)
+        """
+        return self.__paths[dialog_type]
+    
