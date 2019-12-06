17f3440fbf8ea984566f2b0723fba65878b2a4c5
amad3v <amad3v@gmail.com>
2019-12-06 20:55:19 +0000

    removed old modules

--- mods/__init__.py
+++ mods/__init__.py
@@ -1,8 +1,7 @@
-__all__: object = ['fixqt', 'save_xl', 'err_report', 'csvextractor', 'params','csv_extractor']
+__all__: object = ['fixqt', 'save_xl', 'err_report', 'params','csv_extractor']
 
 from . import err_report
 from . import fixqt
-from . import csvextractor
 from . import csv_extractor
 from . import save_xl
 from . import params
